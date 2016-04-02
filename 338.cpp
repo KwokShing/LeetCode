1a96f2004142882540370c058b1dc91d7de154d5
KwokShing <kwokshingchan92@gmail.com>
2016-04-01 23:56:31 -0700

    338

---
+++ 338.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <list>
+#include <cstring>
+#include <queue>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+
+ //Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res(num+1,0);
+        for(int i = 1; i <= num; ++i) {
+            res[i] = res[i&(i-1)]+1;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    ListNode * head = new ListNode(5);
+    head->next = new ListNode(4);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(2);
+    head->next->next->next->next = new ListNode(1);
+    //while(head) {
+    //    cout<<head->val<<endl;
+    //    head = head->next;
+    //}
+    Solution s;
+    cout<<s.getHint("1123","0111")<<endl;
+    
+}
